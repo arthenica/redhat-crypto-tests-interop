648b4648dfa5f47765f2d13fa18fd54524cf352a
Stanislav Zidek <szidek@redhat.com>
2023-12-06 14:20:09 +0100

    explicit about TLS 1.1 breakeage in Fedora 39

--- Interoperability/gnutls_keymatexport-with-OpenSSL/runtest.sh
+++ Interoperability/gnutls_keymatexport-with-OpenSSL/runtest.sh
@@ -76,6 +76,8 @@ rlJournalStart
         declare -a C_OSSLPROTO
 
 
+        # FIXME: TLS 1.1 is broken in crypto-policies in Fedora 39
+        #if [[ $fips -ne 0 ]] && (rlIsRHEL '<9' || rlIsFedora); then
         if [[ $fips -ne 0 ]] && (rlIsRHEL '<9' || rlIsFedora '<39'); then
             C_TEST[$i]="TLS-1.1 SHA1-MD5 PRF"
             C_PRIO[$i]="NONE:+VERS-TLS1.1:+AES-256-CBC:+SHA1:+RSA:+SIGN-ALL"
